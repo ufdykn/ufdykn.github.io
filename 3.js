45546ac610856cc4852eb5ddd664e1ca6a3bc970
ufdykn <64208639+ufdykn@users.noreply.github.com>
2020-08-06 07:23:55 -0500

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,63 @@
+From: <Saved by Blink>
+X-Snapshot-Version: 1.0
+X-Snapshot-Title: =?utf-8?Q??=
+X-Snapshot-Content-Location: http://lib.baomitu.com/html5shiv/3.7.3/html5shiv.min.js
+Subject: 
+Date: Fri, 6 Aug 2020 12:23:14 -0000
+MIME-Version: 1.0
+Content-Type: multipart/related;
+	type="text/html";
+	boundary="----MultipartBoundary--jean4GglXI5G3cTkikZRdtvQuppKwO6h6jP975RxDi----"
+
+------MultipartBoundary--jean4GglXI5G3cTkikZRdtvQuppKwO6h6jP975RxDi----
+Content-Type: text/html
+Content-ID: <frame-16-8ea22ab6-dd30-4da9-990a-1d800e0c065a@mhtml.blink>
+Content-Transfer-Encoding: quoted-printable
+Content-Location: http://lib.baomitu.com/html5shiv/3.7.3/html5shiv.min.js
+
+<html><head><meta http-equiv=3D"Content-Type" content=3D"text/html; charset=
+=3DUTF-8"></head><body><pre style=3D"word-wrap: break-word; white-space: pr=
+e-wrap;">/**
+* @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 =
+Licensed
+*/
+!function(a,b){function c(a,b){var c=3Da.createElement("p"),d=3Da.getElemen=
+tsByTagName("head")[0]||a.documentElement;return c.innerHTML=3D"x&lt;style&=
+gt;"+b+"&lt;/style&gt;",d.insertBefore(c.lastChild,d.firstChild)}function d=
+(){var a=3Dt.elements;return"string"=3D=3Dtypeof a?a.split(" "):a}function =
+e(a,b){var c=3Dt.elements;"string"!=3Dtypeof c&amp;&amp;(c=3Dc.join(" ")),"=
+string"!=3Dtypeof a&amp;&amp;(a=3Da.join(" ")),t.elements=3Dc+" "+a,j(b)}fu=
+nction f(a){var b=3Ds[a[q]];return b||(b=3D{},r++,a[q]=3Dr,s[r]=3Db),b}func=
+tion g(a,c,d){if(c||(c=3Db),l)return c.createElement(a);d||(d=3Df(c));var e=
+;return e=3Dd.cache[a]?d.cache[a].cloneNode():p.test(a)?(d.cache[a]=3Dd.cre=
+ateElem(a)).cloneNode():d.createElem(a),!e.canHaveChildren||o.test(a)||e.ta=
+gUrn?e:d.frag.appendChild(e)}function h(a,c){if(a||(a=3Db),l)return a.creat=
+eDocumentFragment();c=3Dc||f(a);for(var e=3Dc.frag.cloneNode(),g=3D0,h=3Dd(=
+),i=3Dh.length;i&gt;g;g++)e.createElement(h[g]);return e}function i(a,b){b.=
+cache||(b.cache=3D{},b.createElem=3Da.createElement,b.createFrag=3Da.create=
+DocumentFragment,b.frag=3Db.createFrag()),a.createElement=3Dfunction(c){ret=
+urn t.shivMethods?g(c,a,b):b.createElem(c)},a.createDocumentFragment=3DFunc=
+tion("h,f","return function(){var n=3Df.cloneNode(),c=3Dn.createElement;h.s=
+hivMethods&amp;&amp;("+d().join().replace(/[\w\-:]+/g,function(a){return b.=
+createElem(a),b.frag.createElement(a),'c("'+a+'")'})+");return n}")(t,b.fra=
+g)}function j(a){a||(a=3Db);var d=3Df(a);return!t.shivCSS||k||d.hasCSS||(d.=
+hasCSS=3D!!c(a,"article,aside,dialog,figcaption,figure,footer,header,hgroup=
+,main,nav,section{display:block}mark{background:#FF0;color:#000}template{di=
+splay:none}")),l||i(a,d),a}var k,l,m=3D"3.7.3",n=3Da.html5||{},o=3D/^&lt;|^=
+(?:button|map|select|textarea|object|iframe|option|optgroup)$/i,p=3D/^(?:a|=
+b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|t=
+able|tbody|td|th|tr|ul)$/i,q=3D"_html5shiv",r=3D0,s=3D{};!function(){try{va=
+r a=3Db.createElement("a");a.innerHTML=3D"&lt;xyz&gt;&lt;/xyz&gt;",k=3D"hid=
+den"in a,l=3D1=3D=3Da.childNodes.length||function(){b.createElement("a");va=
+r a=3Db.createDocumentFragment();return"undefined"=3D=3Dtypeof a.cloneNode|=
+|"undefined"=3D=3Dtypeof a.createDocumentFragment||"undefined"=3D=3Dtypeof =
+a.createElement}()}catch(c){k=3D!0,l=3D!0}}();var t=3D{elements:n.elements|=
+|"abbr article aside audio bdi canvas data datalist details dialog figcapti=
+on figure footer header hgroup main mark meter nav output picture progress =
+section summary template time video",version:m,shivCSS:n.shivCSS!=3D=3D!1,s=
+upportsUnknownElements:l,shivMethods:n.shivMethods!=3D=3D!1,type:"default",=
+shivDocument:j,createElement:g,createDocumentFragment:h,addElements:e};a.ht=
+ml5=3Dt,j(b),"object"=3D=3Dtypeof module&amp;&amp;module.exports&amp;&amp;(=
+module.exports=3Dt)}("undefined"!=3Dtypeof window?window:this,document);</p=
+re></body></html>
+------MultipartBoundary--jean4GglXI5G3cTkikZRdtvQuppKwO6h6jP975RxDi------
